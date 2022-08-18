9a3581e6c2c818fd2cdca8a4810e43d97e91b122
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-17 23:38:30 -0400

    make new string of CAPS first and last letters co-authored-by: Noah Cowan <nsncowan@gmail.com> co-authored-by: Nicholas Zeng <nickyxzeng@hotmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,14 @@ window.alert("your sentence was " + sentence + ".");
 
 // Business logic
 function capitalize(sentence) {
-  
+  let firstlast = sentence.charAt(0) + sentence.charAt(-1);
+  return firstlast.toUpper();
+}
+
+let ciphFirstLast = capitalize(sentence);
+
+function reverse(ciphFirstLast){
+
 }
 
 
