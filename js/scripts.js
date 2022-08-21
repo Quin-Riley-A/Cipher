8579f2076ad2ede5b048dcbddfcbf7e7343d83ea
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-21 04:32:42 -0400

    complete cipher task and tell user result

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,13 @@ const sentence = window.prompt("Please provide a sentence to be ciphered");
 
 window.alert("your sentence was " + sentence + ".");
 
-window.alert(typeof parseInt(sentence.length))
-
 // Business logic
 function capitalize(sentence, size) {
   const firstlast = sentence.slice(0,1) + sentence.slice(-1);
   return firstlast.toUpperCase();
 }
 
-function reverse(ciphFirstLast){
+function reverseCiph(ciphFirstLast){
   const ciphFirst = ciphFirstLast.slice(-1);
   const ciphLast = ciphFirstLast.slice(0,1);
   return ciphFirst+ciphLast;
@@ -23,9 +21,12 @@ function adder(sent, ciphFirstLast){
   return sent + ciphFirstLast;
 }
 
-function halfPt(){
-
+function halfPt(sent){
+  let half = Math.floor((parseInt(sent.length)/2));
+  return sent.charAt(half-1);
 }
 
 let ciphFirstLast = capitalize(sentence);
-adder(sentence,reverse(ciphFirstLast));
+let cipherSentence = adder(sentence,reverseCiph(ciphFirstLast));
+cipherSentence= halfPt(sentence)+cipherSentence;
+window.alert("Your ciphered sentence is: " + cipherSentence);
